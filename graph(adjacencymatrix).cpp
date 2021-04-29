ce3b306b662232dbafd8a6fd37e279dd10382031
Madhul Raj <62300368+m-raj-glitch@users.noreply.github.com>
2021-04-29 13:27:37 +0530

    Create graph(adjacencymatrix).cpp

---
+++ graph(adjacencymatrix).cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+void print(int**edge,int n, int sv,bool*visited){
+    cout<<sv<<endl;
+    visited[sv]=true;
+    for(int i=0;i<n;i++){
+        if(visited[i]){
+            continue;
+        }
+        if(i==sv){
+            continue;
+        }
+        if(edge[sv][i]==1){
+            print(edge,n,i,visited);
+        }
+    }
+}
+int main(){
+    int e,n;
+    cin>>n>>e;
+    int**edge=new int*[n];
+    for(int i=0;i<n;i++){
+        edge[i]=new int[n];
+        for(int j=0;j<n;j++){
+            edge[i][j]=0;
+        }
+    }
+    for(int i=0;i<e;i++){
+        int f,s;
+        cin>>f>>s;
+        edge[f][s]=1;
+        edge[f][s]=1;
+    }
+    bool*visited=new bool[n];
+    for(int i=0;i<n;i++){
+        visited[i]=false;
+    }
+    print(edge,n,0,visited);
+    return 0;
+}
